1f08e49218e87d8a7e8f5f949c7cdbfe9b564093
rakib <rakibulislamragib2@gmail.com>
2024-10-14 14:31:29 +0600

    Folder Structuring and get started a basic todo App

---
+++ src/Components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const DummyTodo = [
+    {
+        id: 1,
+        title: 'Learn React',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Build an App',
+        completed: true
+    }
+
+]
+function Home() {
+  return (
+    <div>Todo Home</div>
+  )
+}
+
+export default Home
---
+++ src/Components/Todos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todos() {
+  return (
+    <div>
+        Todo folder
+    </div>
+  )
+}
+
+export default Todos
